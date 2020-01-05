985218852729bf5d046b57679ffe64858bc29df1
Bence Baky <zoldsapkas3@gmail.com>
2020-01-05 11:34:43 +0100

    Reordering files, adding new ones as default

---
+++ src/sequence/KREG_USER_TYPES_SEQ.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE kreg_user_types_seq START WITH 1
+                                    MAXVALUE 9999999999999999
+                                    MINVALUE 1
+                                    NOCYCLE
+                                    CACHE 20
+                                    NOORDER
+                                    NOKEEP
+                                    GLOBAL;
