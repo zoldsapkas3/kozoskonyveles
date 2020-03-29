57fbf0def336bf98fb9906c68b9eda464ad50b15
zoldsapkas3 <zoldsapkas3@gmail.com>
2020-03-29 16:59:34 +0200

    Added high res logo
    Removed application files, moved to private repositories under @levnente

--- src/sequence/KREG_USER_PICTURES_SEQ.sql
+++
@@ -1,3 +0,0 @@
-CREATE SEQUENCE kreg_user_pictures_seq START WITH 1
-                                       MINVALUE 1
-                                       MAXVALUE 99999999999999999999;
--- src/sequence/KREG_USER_TYPES_SEQ.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE kreg_user_types_seq START WITH 1
-                                    MAXVALUE 9999999999999999
-                                    MINVALUE 1
-                                    NOCYCLE
-                                    CACHE 20
-                                    NOORDER
-                                    NOKEEP
-                                    GLOBAL;
